161994bb47d5b072c273e227f06c5cd15fdd1b19
Thomas Berriau <thomas.berriau@etu.u-pec.fr>
2016-10-30 17:41:24 +0100

    test validation édition des posts

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -1,3 +1,15 @@
+Template.postEdit.onCreated(function() {
+  Session.set('postEditErrors', {});
+});
+Template.postEdit.helpers({
+  errorMessage: function(field) {
+    return Session.get('postEditErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('postEditErrors')[field] ? 'has-error' : '';
+  }
+});
+
 Template.postEdit.events({
   'submit form': function(e) {
     e.preventDefault();
@@ -8,6 +20,10 @@ Template.postEdit.events({
       url: $(e.target).find('[name=url]').val(),
       title: $(e.target).find('[name=title]').val()
     }
+
+    var errors = validatePost(postProperties);
+    if (errors.title || errors.url)
+      return Session.set('postEditErrors', errors);
 
     Posts.update(currentPostId, {$set: postProperties}, function(error) {
       if (error) {
@@ -22,7 +38,7 @@ Template.postEdit.events({
   'click .delete': function(e) {
     e.preventDefault();
 
-    if (confirm("Delete this post?")) {
+    if (confirm("Supprimer ce post ?")) {
       var currentPostId = this._id;
       Posts.remove(currentPostId);
       Router.go('postsList');
